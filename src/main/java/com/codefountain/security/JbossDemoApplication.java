dddfa82ab09b9e1c62e4222e0490b14ca99193ce
musibs <musib.somnath@gmail.com>
2020-05-26 01:06:14 +1000

    Initial commit

---
+++ src/main/java/com/codefountain/security/JbossDemoApplication.java
@@ -0,0 +1,13 @@
+package com.codefountain.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbossDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbossDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codefountain/security/JbossDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codefountain.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbossDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
